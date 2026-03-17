923e46772566fde66205642dd543e438b466cc47
unknown <privatebird@163.com>
2026-03-17 09:44:18 +0800

    chore: stop tracking local docs and config, update gitignore

--- check_line.py
+++
@@ -1,5 +0,0 @@
-content = open('desktop/app/project_file.py', 'r', encoding='utf-8').read()
-for i, line in enumerate(content.split('\n'), 1):
-    if 'REQUIRED_SECTIONS' in line:
-        print(f'Line {i}: {repr(line)}')
-        print(f'Line {i} bytes: {line.encode("utf-8")}')
